2f45e8bbb44810713930e310816b49d7754cf3eb
Namik02 <Hasannamik02@outlook.com>
2022-07-18 14:53:16 -0700

    Painting Feature Added

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// Function To Create Grid Using Number Inputted Into Size Input
-function createGrid() {
-    const grid = document.querySelector('#grid');
-    const gridSize = document.querySelector('#sizeInput').value
-    // For Loops That creates Horizontal Component Of Grid 
-    for (let i = 1; i <= gridSize; i++) {
-        let outerBox = 'oBox' + i
-
-        const horizontalBox = document.createElement('div');
-        horizontalBox.classList.add('horizontalBox');
-        horizontalBox.classList.add(outerBox);
-        grid.appendChild(horizontalBox);
-
-        let test = '.' + outerBox
-        let inner = document.querySelector(test)
-    // For Loop That Creates Vertical Component Of Grid Within A Horizontal Component
-    for (let i = 1; i <= gridSize; i++) {
-        let innerBox = 'iBox' +1
-        const verticalBox = document.createElement('div');
-        verticalBox.classList.add('verticalBox')
-        verticalBox.classList.add(innerBox)
-            inner.appendChild(verticalBox);
-    }
-    }
-}
-
 // Size Button That Resets Grid(If Used) And Runs The CreateGrid() Function
 const sizeButton = document.querySelector('#sizeButton');
 sizeButton.addEventListener('click', () => {
@@ -33,8 +7,35 @@ sizeButton.addEventListener('click', () => {
 
 
 
+// Function To Create Grid Using Number Inputted Into Size Input
+function createGrid() {
+    const grid = document.querySelector('#grid');
+    const gridSize = document.querySelector('#sizeInput').value
+    // For Loops That creates Horizontal Component Of Grid 
+    for (let i = 1; i <= gridSize; i++) {
+        let outerBox = 'oBox' + i
+        const horizontalBox = document.createElement('div');
+        horizontalBox.classList.add('horizontalBox');
+        horizontalBox.classList.add(outerBox);
+        grid.appendChild(horizontalBox);
 
-// const col = document.querySelector('.box')
-// col.addEventListener('mouseover', () =>{
-    
-// })
+        let test = '.' + outerBox
+        let inner = document.querySelector(test)
+    // For Loop That Creates Vertical Component Of Grid Within A Horizontal Component
+    for (let j = 1; j <= gridSize; j++) {
+        let innerBox = 'iBox' + j
+    let ref = 'gridX' + i + 'Y' + j    
+        const verticalBox = document.createElement('div');
+        verticalBox.classList.add('verticalBox')
+        verticalBox.classList.add(innerBox)
+        verticalBox.classList.add(ref)
+        inner.appendChild(verticalBox);
+
+        tempo = '.' + ref
+        tempo1 = document.querySelector(tempo)
+        tempo1.addEventListener('mouseover', () => {
+            verticalBox.classList.add('hover')
+        });
+    }
+    }
+}
