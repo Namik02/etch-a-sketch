3c4e625588330360d159d35ff9105408660b3e74
Namik02 <Hasannamik02@outlook.com>
2022-07-18 02:22:09 -0700

    inner divs strech within #grid

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const grid = document.querySelector('#grid')
 
-const box = document.createElement('div');
-box.classList.add('box');
 
+
+for (let i = 0; i < 8; i++) {
+    const box = document.createElement('div');
+box.classList.add('box');
 grid.appendChild(box);
+    
+}
+
+// const box = document.createElement('div');
+// box.classList.add('box');
+// grid.appendChild(box);
