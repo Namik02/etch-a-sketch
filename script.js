66a79336d950b0a19e3f4929030d6c13639b38bf
Namik02 <Hasannamik02@outlook.com>
2022-07-18 03:16:49 -0700

    sizing perfected

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const grid = document.querySelector('#grid')
 
-
-
 for (let i = 0; i < 8; i++) {
     const box = document.createElement('div');
-box.classList.add('box');
-grid.appendChild(box);
-    
+    box.classList.add('box');
+    grid.appendChild(box);   
 }
 
-// const box = document.createElement('div');
-// box.classList.add('box');
-// grid.appendChild(box);
+const inner = document.querySelector('.box')
+
+for (let i = 0; i < 8; i++) {
+    const dox = document.createElement('div');
+    dox.classList.add('dox')
+    
+    inner.appendChild(dox); 
+}
