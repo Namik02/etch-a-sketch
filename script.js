669069247f923f51e0956bc11710ad0849264ada
Namik02 <Hasannamik02@outlook.com>
2022-07-18 04:53:22 -0700

    full grid made

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const grid = document.querySelector('#grid')
 
-for (let i = 0; i < 8; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    grid.appendChild(box);   
+// for (let i = 0; i < 8; i++) {
+//     const horizontalBox = document.createElement('div');
+//     horizontalBox.classList.add('horizontalBox');
+//     grid.appendChild(horizontalBox);   
+// }
+
+// const inner = document.querySelector('.horizontalBox')
+
+// for (let i = 0; i < 8; i++) {
+//     const verticalBox = document.createElement('div');
+//     verticalBox.classList.add('verticalBox')
+//     inner.appendChild(verticalBox); 
+// }
+
+
+
+for (let i = 1; i < 9; i++) {
+    let outerBox = 'oBox' + i
+
+    const horizontalBox = document.createElement('div');
+    horizontalBox.classList.add('horizontalBox');
+    horizontalBox.classList.add(outerBox);
+    grid.appendChild(horizontalBox);
+
+    let test = '.' + outerBox
+    let inner = document.querySelector(test)
+
+    for (let i = 1; i < 9; i++) {
+        let innerBox = 'iBox' +1
+        const verticalBox = document.createElement('div');
+        verticalBox.classList.add('verticalBox')
+        verticalBox.classList.add(innerBox)
+        inner.appendChild(verticalBox);
+
+
+    }
 }
 
-const inner = document.querySelector('.box')
 
-for (let i = 0; i < 8; i++) {
-    const dox = document.createElement('div');
-    dox.classList.add('dox')
-    
-    inner.appendChild(dox); 
-}
+
+// const qwe = document.querySelector('.box7')
+// const ex1 = document.createElement('p');
+// ex1.classList.add('ex1');
+// ex1.textContent = 'Hey I’m red!'
+// ex1.style.color = 'red';
+// qwe.appendChild(ex1);;
